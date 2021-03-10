ec2110cd895ffd623b3ae893cea88d68e0c7e668
Denis <dknezevic@collectivemind.dev>
2021-03-10 16:51:11 +0100

    changed the function that returns the postgres connection, changed main so it gets the configuration and the postgres connection

--- main.go
+++ main.go
@@ -4,24 +4,24 @@ import (
 	"net/http"
 
 	"github.com/DenisKnez/muna/handlers"
+	"github.com/DenisKnez/muna/handlers/routers"
 	"github.com/DenisKnez/muna/repositories"
- 	"github.com/DenisKnez/muna/handlers/routers"
 	"github.com/DenisKnez/muna/services"
 	"github.com/DenisKnez/muna/util"
 )
 
 func main() {
 
+	config := util.GetConfig()
+	conn := util.GetDatabaseConnection(config)
 
 	sm := http.NewServeMux()
 
-	ggRepo := repositories.NewGuessGameRepository(util.Db)
+	ggRepo := repositories.NewGuessGameRepository(conn)
 	ggService := services.NewGuessGameService(ggRepo)
 	ggHandler := handlers.NewGuessGameHandler(ggService)
 
-
 	routers.GuessGameRouter(sm, ggHandler)
-
 
 	http.ListenAndServe(":9090", sm)
 
